bf4dce6e29de2f92175ad500e828ab8da9ef7b71
YEO YANSON <180500N@mymail.nyp.edu.sg>
2020-02-05 16:29:30 +0800

    instructions

--- OceanDiver/app/src/main/java/com/example/mgpa1/InstructionPage.java
+++ OceanDiver/app/src/main/java/com/example/mgpa1/InstructionPage.java
@@ -8,9 +8,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class InstructionPage extends AppCompatActivity {
     ListView lst;
-    String[] objects= {"Player", "Obstacles", "Enemy"};
-    String[] desc = {"This is player", "This is obstacle", "This is enemy"};
-    Integer[] imgid={R.drawable.alienblue, R.drawable.dirt, R.drawable.shark};
+    String[] objects= {"Player", "Obstacles", "Enemy", "Coins", "Tap", "Rotate", "Credits"};
+    String[] desc = {"This is player", "This is obstacle", "This is enemy", "This is a coin", "This is the controls: tap", "This is the controls: rotate", "Game made by Yanson and Jagatees"};
+    Integer[] imgid={R.drawable.alienblue, R.drawable.dirt, R.drawable.shark, R.drawable.coint, R.drawable.fingertap, R.drawable.rotate, R.drawable.zombieidle};
     @Override
     protected void onCreate(Bundle savedinstanceState) {
         super.onCreate(savedinstanceState);
